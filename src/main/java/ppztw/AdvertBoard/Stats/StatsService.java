9cc41460400a61e7f091e366af07dc7df6340756
mateuszp <mpftp10@gmail.com>
2019-05-18 08:09:37 +0200

    defined StatsService class and tests for it

---
+++ src/main/java/ppztw/AdvertBoard/Stats/StatsService.java
@@ -0,0 +1,32 @@
+package ppztw.AdvertBoard.Stats;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ppztw.AdvertBoard.Exception.ResourceNotFoundException;
+import ppztw.AdvertBoard.Model.Advert.AdvertStats;
+import ppztw.AdvertBoard.Repository.Advert.AdvertStatsRepository;
+
+@Service
+public class StatsService {
+
+    @Autowired
+    private AdvertStatsRepository advertStatsRepository;
+
+    public void addAdvertEntry(Long advertId) {
+        AdvertStats advertStats = findAdvertStatsById(advertId);
+        advertStats.setEntryCount(advertStats.getEntryCount() + 1);
+        advertStatsRepository.save(advertStats);
+    }
+
+    public Integer getAdvertEntryCount(Long advertId) {
+        AdvertStats advertStats = findAdvertStatsById(advertId);
+        return advertStats.getEntryCount();
+    }
+
+
+    private AdvertStats findAdvertStatsById(Long advertId) {
+        return advertStatsRepository.findById(advertId).orElseThrow(() ->
+                new ResourceNotFoundException("Advert", "id", advertId));
+    }
+}
---
+++ src/test/java/ppztw/AdvertBoard/StatsServiceTests.java
@@ -0,0 +1,63 @@
+package ppztw.AdvertBoard;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import ppztw.AdvertBoard.Model.Advert.AdvertStats;
+import ppztw.AdvertBoard.Repository.Advert.AdvertStatsRepository;
+import ppztw.AdvertBoard.Stats.StatsService;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class StatsServiceTests {
+
+    @Autowired
+    private StatsService statsService;
+
+    @MockBean
+    private AdvertStatsRepository advertStatsRepository;
+
+    @Before
+    public void setUp() {
+        AdvertStats advertStats = new AdvertStats(0L, null, 0);
+        Mockito.when(advertStatsRepository.findById(0L)).thenReturn(Optional.of(advertStats));
+    }
+
+    @Test
+    public void getAdvertEntryCount_ExistingIdGiven_ShouldReturnNonNullValue() {
+        Long advertId = 0L;
+
+        assertThat(statsService.getAdvertEntryCount(advertId)).isNotNull();
+    }
+
+    @Test
+    public void addAdvertEntry_ShouldIncreaseEntryCount() {
+        Long advertId = 0L;
+        Integer beforeEntryCount = statsService.getAdvertEntryCount(advertId);
+        statsService.addAdvertEntry(advertId);
+        Integer afterEntryCount = statsService.getAdvertEntryCount(advertId);
+
+        assertThat(afterEntryCount).isEqualTo(beforeEntryCount + 1);
+    }
+
+    @TestConfiguration
+    static class StatsServiceTestsContextConfiguration {
+        @Bean
+        public StatsService statsService() {
+            return new StatsService();
+        }
+    }
+
+
+}
